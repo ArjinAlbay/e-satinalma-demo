db97b0a4e473887fd7beb1136dace8fa9170e0f2
arjin <arjinalbay21@gmail.com>
2025-02-18 00:05:38 +0300

    delete icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { MapPin, Phone, Mail } from "lucide-react";
+import { MapPin, Phone } from "lucide-react";
 
 const Footer = () => {
   const menuItems = [
